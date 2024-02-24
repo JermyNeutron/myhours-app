81232ee1b03bd567acafa8271eb28b7ec4c6b398
Jeremy Crisostomo <jeremycrisostomo@gmail.com>
2024-02-24 08:37:33 -0800

    updated authentication to write credentials.txt and verify successful login attempt

--- authenticate.py
+++ authenticate.py
@@ -6,43 +6,44 @@ import json
 import requests
 import time
 
+
 # check config population
 def config_check():
-    
+
     # # start clock
     # time_start = time.time()
 
     config = configparser.ConfigParser()
-    config.read('test_config.ini')
-    # live: config.ini
+    config.read("test_config.ini")
+    # LIVE: config.ini
 
     global missing_authentication
     missing_authentication = 0
 
     # retrieve email
-    if config.has_option('MyHours', 'login_email'):
-        ini_email = config.get('MyHours', 'login_email')
-        if ini_email == '':
-            print("Oops! Open config.ini and provide an email.", end=' ')
+    if config.has_option("MyHours", "login_email"):
+        ini_email = config.get("MyHours", "login_email")
+        if ini_email == "":
+            print("Oops! Open config.ini and provide an email.", end=" ")
             missing_authentication += 1
         else:
             # print(f"Email is {ini_email}", end=' ') # OPTIONAL
             email_populated = ini_email
     else:
-        print('The email key is missing!')
+        print("The email key is missing!")
         missing_authentication += 1
 
     # retrieve password
-    if config.has_option('MyHours', 'login_password'):
-        ini_password = config.get('MyHours', 'login_password')
-        if ini_password == '':
+    if config.has_option("MyHours", "login_password"):
+        ini_password = config.get("MyHours", "login_password")
+        if ini_password == "":
             print("Oops! Open config.ini and provide a password.")
             missing_authentication += 1
         else:
             # print(f"Password is stored.") # OPTIONAL
             password_provided = ini_password
     else:
-        print('The password key is missing!')
+        print("The password key is missing!")
         missing_authentication += 1
 
     # # end clock
@@ -51,35 +52,56 @@ def config_check():
 
     # exit function
     if missing_authentication > 0:
-        return print(
-            'Please resolve your config.ini'
-        )
-    
+        return print("Please resolve your config.ini")
+
     # continue function
     return email_populated, password_provided
+
 
 # user authentication and bearer token retrieval
 def mh_login(email, password):
     url = "https://api2.myhours.com/api/tokens/login"
 
-    payload = json.dumps({
-    "grantType": "password",
-    "email": f"{email}",
-    "password": f"{password}",
-    "clientId": "api"
-    })
-    headers = {
-    'Content-Type': 'application/json',
-    'api-version': '1.0'
-    }
+    payload = json.dumps(
+        {
+            "grantType": "password",
+            "email": f"{email}",
+            "password": f"{password}",
+            "clientId": "api",
+        }
+    )
+    headers = {"Content-Type": "application/json", "api-version": "1.0"}
 
     # Calls bearer token
     pre_response = requests.request("POST", url, headers=headers, data=payload)
     response = pre_response.json()
-    print(response) # OPTIONAL
+    if "message" in response:
+        print("Incorrect email or password. Review the config.ini")
+        return None
+    else:
+        # print(response)  # OPTIONAL
+        # print(type(response))  # OPTIONAL
+        return response
+
+
+# overwriting credentials file
+def writing_response(
+    response, credentials_file="test_credentials.txt"
+):  # LIVE: credentials.txt
+    with open(credentials_file, "w") as file:
+        json.dump(response, file)
+
+
+# check credentials file # OPTIONAL
+def checking_response(credentials_file):
+    with open(credentials_file, "r") as file:
+        print(
+            f"This is read from the .txt file:\n{file.read()}\nCredentials successfully retrieved."
+        )  # OPTIONAL
+
 
 # testing purposes
-if __name__ == '__main__':
+if __name__ == "__main__":
     # start time # optional
     time_start = time.time()
 
@@ -87,8 +109,13 @@ if __name__ == '__main__':
     email, password = config_check()
     if not missing_authentication:
         # user authentication and bearer token retrieval
-        mh_login(email, password)
+        response = mh_login(email, password)
+        if response is None:
+            print("Program has exited.")
+        else:
+            writing_response(response)
+            checking_response("test_credentials.txt")  # OPTIONAL
 
     # end clock # optional
     time_end = time.time()
-    print(f'{time_end - time_start}s elapsed')
+    print(f"{time_end - time_start}s elapsed")
