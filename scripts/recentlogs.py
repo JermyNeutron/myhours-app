616f3da9274c4e41b620ed9abb17b21aefd8e29d
Jeremy Crisostomo <jeremycrisostomo@gmail.com>
2024-03-04 10:51:14 -0800

    update recentlogs print statements

--- scripts/recentlogs.py
+++ scripts/recentlogs.py
@@ -9,17 +9,13 @@ sys.path.append('.')
 
 from src.timer_class import sessionCurrent
 
-current_time_iso = ''
-
 def timestamp():
     current_time_utc = datetime.utcnow().replace(tzinfo=pytz.utc)
     pst = pytz.timezone('America/Los_Angeles')
     current_time_pst = current_time_utc.astimezone(pst)
-    global current_time_iso
     current_time_iso = current_time_pst.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + 'Z'
     # print(f"timestamp func returns: {current_time_iso}") # OPTIONAL
     current_time_iso = current_time_iso[:10]
-    print(current_time_iso)
     return current_time_iso
 
 def requestlog():
@@ -27,10 +23,6 @@ def requestlog():
         # LIVE: temp/credentials.txt
         json_data = file.read()
     mydictionary = json.loads(json_data)
-    # print(mydictionary)
-    # atok = mydictionary["accessToken"]
-
-    # print(atok)
 
     payload = {}
     headers = {
@@ -43,12 +35,12 @@ def requestlog():
 
     # print(type(response.json())) # list
     for dictionary in response.json():
-        print(sessionCurrent(dictionary))
+        print(f"\n{sessionCurrent(dictionary)}\n")
 
 if __name__ == '__main__':
-    timestamp()
+    current_date = timestamp()
 
-    url = f"https://api2.myhours.com/api/Logs?date={current_time_iso}&step=100"
-    print(url)
+    url = f"https://api2.myhours.com/api/Logs?date={current_date}&step=100"
+    # print(url) # testing
 
     requestlog()
