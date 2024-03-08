7421b026fe00e32fa9c90dbfcaddc994192c8b3f
Jeremy Desktop <jeremycrisostomo@gmail.com>
2024-03-07 23:18:29 -0800

    updated main_cli to stop timer upon closing

--- scripts/main_cli.py
+++ scripts/main_cli.py
@@ -13,12 +13,19 @@ def clear_screen():
     else:
         _ = os.system('cls')
 
+def main_screen_options():
+    pass
+
 def timer_start_options():
     clear_screen()
     while True:
         proj_list = get_projects.display_list()
 
         usel_sub = input('\nHit <Enter> to start a blank timer\n\nType in your project: ')
+
+        if usel_sub == '':
+            usel_sub = None
+            projectId = None
 
         if usel_sub is not None:
             for proj in proj_list:
@@ -30,26 +37,42 @@ def timer_start_options():
                 print("Invalid selection.")
         return projectId
 
+def timer_statement(status):
+    if status is False:
+        return "Let's get cooking!"
+    else:
+        return "Can't Stop! Won't Stop"
+
 def main_screen():
     get_projects.main()
+    # initialize status
+    timer_status = False
     while True:
+        statement = timer_statement(timer_status)
         clear_screen()
-        print("""MyHours App
-              
+        print(f"""MyHours App
+{statement}
+
 Select an option:
 1. Start Timer,
 2. Stop Timer,
               
 Q. Exit""")
         usel = input(': ')
-        if usel == 'q':
+        if usel.lower() == 'q':
+            if timer_status:
+                timer_stop.main()
             break
         elif usel == '1':
             projectId = timer_start_options()
             timer_start.main(projectId)
+            timer_status = True
+            print(timer_status)
+            time.sleep(2)
         elif usel == '2':
             timer_stop.main()
-            time.sleep(5)
+            timer_status = False
+            time.sleep(4)
             clear_screen()
         
         
