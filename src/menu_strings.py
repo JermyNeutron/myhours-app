dca854cbf9415a3ef2c70e5e0365e18c2dad0c86
Jeremy Desktop <jeremycrisostomo@gmail.com>
2024-03-10 20:26:10 -0700

    separated menu options

---
+++ src/menu_strings.py
@@ -0,0 +1,23 @@
+def main(status, test=False):
+    menu_live = f"""MyHours App
+{status}
+Type an option:
+1) Start Timer
+2) Stop Timer
+
+Q) Save and Close
+"""
+    menu_testing = f"""MyHours App
+{status}
+
+Type an option:
+1) Start Timer
+2) Stop Timer
+
+Test) Clean Exit
+
+Q) Save and Close"""
+    return menu_live if not test else menu_testing
+
+if __name__ == '__main__':
+    print(main("",test=True))
