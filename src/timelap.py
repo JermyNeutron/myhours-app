3ec1c149d4a2ae7e29a033656af1514195404ce5
Jeremy Desktop <jeremycrisostomo@gmail.com>
2024-03-10 20:55:10 -0700

    updated timelap return statement

--- src/timelap.py
+++ src/timelap.py
@@ -6,6 +6,6 @@ def timelap(func):
         time_start = time.time()
         result = func(*args, **kwargs)
         time_stop = time.time()
-        print(f"Current Lap: {(time_stop - time_start):.4f}s")
+        print(f"Lap for {func.__name__}: {(time_stop - time_start):.4f}s")
         return result
     return wrapper
