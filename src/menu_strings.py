3e2be328b4c2d519cfdd9af8eef9e9ef4ee4cab3
Jeremy Crisostomo <jeremycrisostomo@gmail.com>
2024-03-13 01:24:23 -0700

    updated menu_strings

--- src/menu_strings.py
+++ src/menu_strings.py
@@ -1,24 +1,26 @@
-def main(status, test=False):
-    menu_live = f"""MyHours App
-{status}
-Type an option:
+def main(test=False):
+    menu_live = """Type an option:
 1) Start Timer
 2) Stop Timer
+3) Retrieve Recent Logs
+
+9) Refresh
 
 Q) Save and Close
 """
-    menu_testing = f"""MyHours App
-{status}
-
-Type an option:
+    menu_testing = """Type an option:
 1) Start Timer
 2) Stop Timer
+3) Retrieve Recent Logs
+
+9) Refresh
 
 Test) Clean Exit
-
 Q) Save and Close"""
+    menu_live_active = """"""
+    menu_testing_active = """"""
     return menu_live if not test else menu_testing
 
 
 if __name__ == '__main__':
-    print(main("",test=True))
+    print(main(test=True))
